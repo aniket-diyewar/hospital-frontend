37b65537266b3d49861c69b3655d7d7fc9981028
aniket-diyewar <aniket.diyewar05@gmail.com>
2026-05-26 11:55:59 +0530

    frontend initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+
+  const [patients, setPatients] = useState([]);
+
+  useEffect(() => {
+    fetchPatients();
+  }, []);
+
+  const fetchPatients = async () => {
+    const response = await axios.get("http://localhost:8080/patients");
+    setPatients(response.data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: "20px" }}>
+      <h1>Hospital Management System</h1>
+
+      {patients.map((patient) => (
+        <div key={patient.id}>
+          <h3>{patient.name}</h3>
+          <p>Age: {patient.age}</p>
+          <p>Disease: {patient.disease}</p>
+          <hr />
+        </div>
+      ))}
     </div>
   );
 }
